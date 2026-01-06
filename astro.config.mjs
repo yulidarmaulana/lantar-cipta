cc4367f481da0ebc11a1d1c4bcf93125769cffec
yulidar maulana ivan saputra <31232250+yulidarmaulana@users.noreply.github.com>
2026-01-06 08:59:51 +0700

    Fitur: Konfigurasikan Astro untuk deployment GitHub Pages dan tambahkan alur kerja GitHub Actions.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://yulidar.github.io/lantar-cipta',
+  base: '/lantar-cipta',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
